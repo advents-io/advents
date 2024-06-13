b5d0859e84a97c5d27848d730c87b58b11bff794
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-06-12 23:11:20 -0300

    feat: configura rota de insertLog

--- src/routes/logs.ts
+++ src/routes/logs.ts
@@ -1,16 +1,29 @@
 import { FastifyInstance } from 'fastify'
-import { Prisma } from '@prisma/client'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
 
-import { prisma } from '@/lib/prisma'
+import { PrismaJson, prisma } from '@/lib/prisma'
 
 export async function insertLog(app: FastifyInstance) {
-  app.post('/logs', async (request, reply) => {
-    await prisma.log.create({
-      data: {
-        log: request.body as Prisma.InputJsonValue,
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/logs',
+    {
+      schema: {
+        body: z.object({
+          comment: z.string().nullable(),
+          data: z.string(),
+        }),
       },
-    })
+    },
+    async (request, reply) => {
+      await prisma.log.create({
+        data: {
+          comment: request.body.comment,
+          data: request.body.data as PrismaJson,
+        },
+      })
 
-    reply.status(201)
-  })
+      reply.status(201)
+    },
+  )
 }
