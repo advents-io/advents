2d6305aa3f9676e052419a88455e51886595119f
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-06-14 13:51:16 -0300

    feat: altera log para ter os dados do dispositivo

--- prisma/migrations/20240613015108_create_database_structure/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "logs" (
-    "id" TEXT NOT NULL,
-    "comment" TEXT,
-    "data" JSONB NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "logs_pkey" PRIMARY KEY ("id")
-);
--- src/routes/logs.ts
+++ src/routes/logs.ts
@@ -4,26 +4,43 @@ import { z } from 'zod'
 
 import { PrismaJson, prisma } from '@/lib/prisma'
 
+const insertLogSchema = z.object({
+  comment: z.string().nullable(),
+  data: z.unknown(),
+
+  deviceId: z.string().nullable(),
+  deviceName: z.string().nullable(),
+  deviceBrand: z.string().nullable(),
+  deviceModel: z.string().nullable(),
+  deviceYearClass: z.string().nullable(),
+  osName: z.string().nullable(),
+  osVersion: z.string().nullable(),
+  installTime: z.date().nullable(),
+  installReferrer: z.string().nullable(),
+  appVersion: z.string().nullable(),
+})
+
 export async function insertLog(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/logs',
     {
       schema: {
-        body: z.object({
-          comment: z.string().nullable(),
-          data: z.unknown(),
-        }),
+        body: insertLogSchema,
       },
     },
     async (request, reply) => {
-      await prisma.log.create({
-        data: {
-          comment: request.body.comment,
-          data: request.body.data as PrismaJson,
-        },
-      })
+      try {
+        await prisma.log.create({
+          data: {
+            ...request.body,
+            data: request.body.data as PrismaJson,
+          },
+        })
 
-      reply.status(201)
+        reply.status(201)
+      } catch (error) {
+        reply.status(400)
+      }
     },
   )
 }
