369288a64e3e6d0ccfebdf931c004ddcde153ef0
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-06-14 14:13:03 -0300

    feat: adiciona log das requests em dev

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { ZodTypeProvider, serializerCompiler, validatorCompiler } from 'fastify-
 
 import { insertLog } from '@/routes/logs'
 import { checkServerStatus } from '@/routes'
+import { isDevelopment } from '@/utils/environment'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -13,6 +14,13 @@ app.register(fastifyCors)
 
 app.register(insertLog)
 app.register(checkServerStatus)
+
+if (isDevelopment) {
+  app.addHook('onRequest', (request, _, done) => {
+    console.log(`${request.method} ${request.url}`)
+    done()
+  })
+}
 
 const port = Number(process.env.PORT) || 3000
 const host = 'RENDER' in process.env ? `0.0.0.0` : `localhost`
