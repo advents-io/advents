df10e9eba4f2965daa59a6fa44babeb16cbeb15c
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-07-20 15:15:41 -0300

    feat: remove api

--- apps/api/src/routes/get-server-status.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from 'fastify'
-
-export default async function (app: FastifyInstance) {
-  app.get('/', (_, reply) => {
-    reply.send({
-      status: 'Server is running!',
-    })
-  })
-}
