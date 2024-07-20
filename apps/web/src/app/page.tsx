2ee64adf2f1d99212e1b9509eeeeb4b908d0365f
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-07-20 15:18:08 -0300

    feat: cria action para criar links

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,26 @@
+'use client'
+
+import { prisma } from '@advents/prisma'
+
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <main className='flex min-h-screen items-center justify-center'>Advents</main>
+  const createLink = async () => {
+    await prisma.link.create({
+      data: {
+        domain: 'l.advents.io',
+        slug: 'teste2',
+        iosUrl: 'https://apps.apple.com/br/app/id1598991618?platform=iphone',
+        androidUrl: 'https://play.google.com/store/apps/details?id=com.quebarbada.quebarbada',
+        fallbackUrl: 'https://favorito.digital',
+      },
+    })
+  }
+
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-center gap-5'>
+      Advents
+      <Button onClick={createLink}>Click me</Button>
+    </main>
+  )
 }
