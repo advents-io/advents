22444c34f02692f2595a4fcdc1efbb15636f9494
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-07-22 01:23:03 -0300

    feat: cria middleware de redirecionamento de links

---
+++ apps/app/src/middleware.ts
@@ -0,0 +1,39 @@
+import { prisma } from '@advents/prisma'
+import { NextRequest, NextResponse } from 'next/server'
+
+import { LINK_DOMAINS, WEBSITE_URL } from '@/utils/constants'
+
+export default async function middleware(req: NextRequest) {
+  const domain = (req.headers.get('host') as string).replace('www.', '').toLowerCase()
+
+  if (!LINK_DOMAINS.includes(domain)) {
+    return NextResponse.next()
+  }
+
+  const slug = req.nextUrl.pathname.split('/')[1]
+
+  if (!slug) {
+    return NextResponse.redirect(WEBSITE_URL, {
+      status: 302,
+    })
+  }
+
+  const link = await prisma.link.findUnique({
+    where: {
+      domain_slug: {
+        domain,
+        slug,
+      },
+    },
+  })
+
+  if (!link) {
+    return NextResponse.redirect(WEBSITE_URL, {
+      status: 302,
+    })
+  }
+
+  return NextResponse.redirect(link.fallbackUrl, {
+    status: 302,
+  })
+}
