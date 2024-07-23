c855b348d140eda0f989cdb4a5d2c355e7db19d2
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-07-22 23:45:33 -0300

    refac: refac da server action

--- apps/app/src/actions/create-link.ts
+++ apps/app/src/actions/create-link.ts
@@ -2,11 +2,11 @@
 
 import { prisma } from '@/lib/prisma'
 
-export async function createLink() {
+export const createLink = async () => {
   await prisma.link.create({
     data: {
       domain: 'l.advents.io',
-      slug: 'teste2',
+      slug: 'teste3',
       iosUrl: 'https://apps.apple.com/br/app/id1598991618?platform=iphone',
       androidUrl: 'https://play.google.com/store/apps/details?id=com.quebarbada.quebarbada',
       fallbackUrl: 'https://favorito.digital',
