4388aaab9d72da32aded7db3f7dd3ecc8f7cbb40
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-07-23 19:59:58 -0300

    feat: configura header

--- apps/app/src/components/header.tsx
+++ apps/app/src/components/header.tsx
@@ -1,34 +1,55 @@
+'use client'
+
 import { Menu, Package2 } from 'lucide-react'
 import Link from 'next/link'
+import { useState } from 'react'
 
+import { NavigationItem } from '@/components/navigation-item'
 import { Button } from '@/components/ui/button'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { Routes } from '@/utils/routes'
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const closeMenu = () => setIsMenuOpen(false)
+
   return (
     <header className='sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6'>
-      <nav className='hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6'>
-        <Link href='#' className='flex items-center gap-2 text-lg font-semibold md:text-base'>
+      <nav className='hidden flex-row items-center gap-6 md:flex'>
+        <Link href={Routes['/']}>
           <Package2 className='h-6 w-6' />
         </Link>
-        <Link href='#' className='transition-colors hover:text-foreground'>
-          Links
-        </Link>
+
+        <NavigationItem href={Routes['/']}>Links</NavigationItem>
+        <NavigationItem href={Routes['/settings']}>Settings</NavigationItem>
       </nav>
-      <Sheet>
+
+      <Sheet open={isMenuOpen} onOpenChange={setIsMenuOpen}>
         <SheetTrigger asChild>
-          <Button variant='outline' size='icon' className='shrink-0 md:hidden'>
-            <Menu className='h-5 w-5' />
-          </Button>
-        </SheetTrigger>
-        <SheetContent side='left'>
-          <nav className='grid gap-6 text-lg font-medium'>
-            <Link href='#' className='flex items-center gap-2 text-lg font-semibold'>
+          <div className='flex flex-1 items-center justify-between md:hidden'>
+            <Link href={Routes['/']}>
               <Package2 className='h-6 w-6' />
             </Link>
-            <Link href='#' className='hover:text-foreground'>
-              Links
+
+            <Button variant='outline' size='icon'>
+              <Menu className='h-5 w-5' />
+            </Button>
+          </div>
+        </SheetTrigger>
+
+        <SheetContent side='right'>
+          <nav className='grid gap-6'>
+            <Link href={Routes['/']} className='flex items-center gap-2 text-lg font-semibold'>
+              <Package2 className='h-6 w-6' />
             </Link>
+
+            <NavigationItem onClick={closeMenu} href={Routes['/']}>
+              Links
+            </NavigationItem>
+            <NavigationItem onClick={closeMenu} href={Routes['/settings']}>
+              Settings
+            </NavigationItem>
           </nav>
         </SheetContent>
       </Sheet>
