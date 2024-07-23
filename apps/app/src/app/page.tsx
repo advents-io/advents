1751a6600c82227721de2083ae44842b55e5093b
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-07-23 14:48:10 -0300

    feat: cria a listagem de links

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -1,13 +1,26 @@
-import { createLink } from '@/actions/create-link'
+import { LinkItem } from '@/components/link-item'
 import { Button } from '@/components/ui/button'
+import { prisma } from '@/lib/prisma'
 
-export default function Home() {
+export default async function Home() {
+  const links = await prisma.link.findMany()
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center gap-5'>
-      Advents
-      <form action={createLink}>
-        <Button type='submit'>Click me</Button>
-      </form>
+    <main className='flex flex-1 flex-col p-8 md:p-14'>
+      <div className='mb-4 flex items-center justify-between'>
+        <h1 className='text-xl font-bold'>Links</h1>
+        <Button variant='default' size='sm'>
+          Create link
+        </Button>
+      </div>
+      <div className='space-y-4'>
+        {links.map(link => (
+          <LinkItem key={link.id} link={link} />
+        ))}
+      </div>
+      <p className='mt-4 text-sm text-muted-foreground'>
+        Showing 1 - {links.length} of {links.length} links
+      </p>
     </main>
   )
 }
---
+++ apps/app/src/components/link-item.tsx
@@ -0,0 +1,34 @@
+import { Link } from '@prisma/client'
+import NextLink from 'next/link'
+
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+
+interface Props {
+  link: Link
+}
+
+export const LinkItem = ({ link }: Props) => {
+  const fullLink = `${link.domain}/${link.slug}`
+  const fullHttpsLink = `https://${fullLink}`
+
+  return (
+    <Card>
+      <CardHeader>
+        <NextLink href={fullHttpsLink} className='font-semibold' target='_blank'>
+          {fullLink}
+        </NextLink>
+      </CardHeader>
+      <CardContent>
+        <p className='text-sm text-muted-foreground'>
+          <b>Fallback:</b> <NextLink href={link.fallbackUrl}>{link.fallbackUrl}</NextLink>
+        </p>
+        <p className='text-sm text-muted-foreground'>
+          <b>iOS:</b> <NextLink href={link.iosUrl}>{link.iosUrl}</NextLink>
+        </p>
+        <p className='text-sm text-muted-foreground'>
+          <b>Android:</b> <NextLink href={link.androidUrl}>{link.androidUrl}</NextLink>
+        </p>
+      </CardContent>
+    </Card>
+  )
+}
