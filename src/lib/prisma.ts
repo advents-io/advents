e5276c8347e2fe81db36796647ff46dcbd5975f2
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-07-30 01:00:09 -0300

    feat: altera o db para o supabase novamente

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,3 @@
-import { Pool } from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient } from '@prisma/client'
 
-const neon = new Pool({ connectionString: process.env.DATABASE_URL })
-const adapter = new PrismaNeon(neon)
-const prisma = new PrismaClient({ adapter })
-
-export { prisma }
+export const prisma = new PrismaClient()
