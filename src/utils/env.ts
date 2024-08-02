d87583357c450cc5f37f9b862430b95c490743d0
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-08-02 00:17:48 -0300

    fix: corrige erros de variáveis de ambiente no client side

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -7,18 +7,16 @@ const envSchema = z.object({
   SUPABASE_ANON_KEY: z.string(),
 })
 
-export const envParse = envSchema.safeParse(process.env)
+const serverRuntime = typeof window === 'undefined'
 
-if (!envParse.success) {
-  console.error(envParse.error)
-}
-
-export const env = envParse.data || {
-  DATABASE_URL: '',
-  DIRECT_URL: '',
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: '',
-}
+export const env = serverRuntime
+  ? envSchema.parse(process.env)
+  : {
+      DATABASE_URL: '',
+      DIRECT_URL: '',
+      SUPABASE_URL: '',
+      SUPABASE_ANON_KEY: '',
+    }
 
 export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development'
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production'
