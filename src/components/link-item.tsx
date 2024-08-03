f093e7f73b9284ea0ba1d824a5e929b20d80caef
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-08-02 23:13:14 -0300

    feat: adiciona botão para copiar o short link

--- src/components/link-item.tsx
+++ src/components/link-item.tsx
@@ -1,28 +1,57 @@
+'use client'
+
 import { Link } from '@prisma/client'
+import { Copy } from 'lucide-react'
 import NextLink from 'next/link'
 
 import { dayjs } from '@/lib/dayjs'
+import { Button } from '@/ui/button'
 import { Card, CardContent } from '@/ui/card'
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/ui/tooltip'
+import { useToast } from '@/ui/use-toast'
 
 interface Props {
   link: Pick<Link, 'domain' | 'slug' | 'createdAt'>
 }
 
 export const LinkItem = ({ link }: Props) => {
+  const { toast } = useToast()
+
   const shortLink = `${link.domain}/${link.slug}`
   const httpShortLink = `https://${shortLink}`
 
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(httpShortLink)
+
+    toast({
+      title: 'Link copiado',
+    })
+  }
+
   return (
     <Card>
-      <CardContent className='flex gap-4 p-6 text-sm'>
+      <CardContent className='flex items-center gap-2 p-6 text-sm'>
         <NextLink href={httpShortLink} className='font-semibold' target='_blank'>
           {shortLink}
         </NextLink>
 
         <Tooltip>
           <TooltipTrigger asChild>
-            <span className='text-muted-foreground'>{formatDate(link.createdAt)}</span>
+            <Button
+              className='h-8 w-8 rounded-full'
+              onClick={copyToClipboard}
+              variant='ghost'
+              size='icon'
+            >
+              <Copy className='h-4 w-4' />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>Copiar link</TooltipContent>
+        </Tooltip>
+
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <span className='ml-2 text-muted-foreground'>{formatDate(link.createdAt)}</span>
           </TooltipTrigger>
           <TooltipContent>{dayjs(link.createdAt).format('DD MMM YY, HH:mm')}</TooltipContent>
         </Tooltip>
