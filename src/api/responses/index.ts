172e20f1d1e8ea00979576379d84e5622a48465f
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-08-03 01:37:38 -0300

    feat: cria http response ok

--- src/api/responses/index.ts
+++ src/api/responses/index.ts
@@ -1,5 +1,6 @@
 import { badRequest } from './bad-request'
 import { created } from './created'
 import { internalServerError } from './internal-server-error'
+import { ok } from './ok'
 
-export { badRequest, created, internalServerError }
+export { badRequest, created, internalServerError, ok }
