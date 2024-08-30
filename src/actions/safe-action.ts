f072178ada9e908cbf6660e14b56c302b57e3cfd
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-08-30 10:11:50 -0300

    refac: altera local do safe-action

---
+++ src/actions/safe-action.ts
@@ -0,0 +1,48 @@
+import { Prisma } from '@prisma/client'
+import { createSafeActionClient } from 'next-safe-action'
+
+import { ActionError } from './action-error'
+
+export const actionClient = createSafeActionClient({
+  handleReturnedServerError(e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      return e.code === 'P2002'
+        ? `Já existe um registro com o mesmo valor único (${e.code})`
+        : `Erro ao processar a requisição (Erro ${e.code})`
+    }
+
+    if (e instanceof ActionError) {
+      return e.message
+    }
+
+    return 'Ocorreu um erro inesperado. Tente novamente ou entre em contato com o suporte.'
+  },
+  defaultValidationErrorsShape: 'flattened',
+})
+
+export const formatErrors = (result: {
+  serverError?: string
+  validationErrors?: {
+    formErrors: string[]
+    fieldErrors: Record<string, string[]>
+  }
+}) => {
+  const { serverError, validationErrors } = result
+  const errors: string[] = []
+
+  if (serverError) {
+    errors.push(serverError)
+  }
+
+  if (validationErrors?.fieldErrors) {
+    Object.values(validationErrors.fieldErrors).forEach(fieldErrors => {
+      errors.push(...fieldErrors)
+    })
+  }
+
+  if (validationErrors?.formErrors) {
+    errors.push(...validationErrors.formErrors)
+  }
+
+  return errors.join('\n')
+}
