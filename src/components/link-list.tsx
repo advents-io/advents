9c077b824456f1969304a22f877c5d1e15dfd7f0
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-09-02 16:55:01 -0300

    feat: adiciona aviso de nenhum link criado

--- src/components/link-list.tsx
+++ src/components/link-list.tsx
@@ -37,13 +37,22 @@ export const LinkList = async ({ page, appId }: Props) => {
 
   return (
     <>
+      {links.length === 0 && (
+        <div className='py-8 text-center'>
+          <p className='text-lg'>Nenhum link encontrado.</p>
+          <p className='mt-4 text-sm text-muted-foreground'>
+            Clique no botão &quot;Criar link&quot; para começar.
+          </p>
+        </div>
+      )}
+
       <div className='space-y-4'>
         {links.map((link, index) => (
           <LinkItem key={index} link={link} />
         ))}
       </div>
 
-      <LinksPagination page={page} pageSize={pageSize} total={totalLinks} />
+      {links.length > 0 && <LinksPagination page={page} pageSize={pageSize} total={totalLinks} />}
     </>
   )
 }
--- src/components/loading-link-list.tsx
+++ src/components/loading-link-list.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from 'lucide-react'
 
 export const LoadingLinkList = () => {
   return (
-    <div className='flex flex-1 items-center justify-center gap-2'>
+    <div className='flex items-center justify-center gap-2'>
       <Loader2 className='size-6 animate-spin' />
       Carregando...
     </div>
