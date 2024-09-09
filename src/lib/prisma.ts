1d1a1f868f85f0bd06be75fabb7c6d7a7ed53cbd
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-09-09 10:53:43 -0300

    fix: corrige erros de max connections no prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
+import { IS_DEVELOPMENT } from '@/utils/env'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>
+} & typeof global
+
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+if (IS_DEVELOPMENT) {
+  globalThis.prismaGlobal = prisma
+}
