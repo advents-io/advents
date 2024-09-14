d5ef8a9b9312c71e933502b0e0cfe1f11e062aac
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-09-13 23:20:49 -0300

    feat: adiciona página para mostrar a chave da api

---
+++ src/app/(private)/[team]/[app]/settings/sdk/loading.tsx
@@ -0,0 +1,5 @@
+import { LoadingPageContent } from '@/components/loading-page-content'
+
+export default function SdkLoading() {
+  return <LoadingPageContent className='mt-10' />
+}
--- src/app/(private)/[team]/[app]/settings/sdk/page.tsx
+++ src/app/(private)/[team]/[app]/settings/sdk/page.tsx
@@ -1,3 +1,26 @@
-export default function Sdk() {
-  return <div>Sdk</div>
+import { ApiKeyField } from '@/components/api-key-field'
+import { ErrorAlert } from '@/components/error-alert'
+import { prisma } from '@/lib/prisma'
+
+export default async function Sdk({ params }: { params: { app: string } }) {
+  const apiKey = await prisma.apiKey.findFirst({
+    where: {
+      app: {
+        slug: params.app,
+      },
+    },
+    select: {
+      key: true,
+    },
+  })
+
+  if (!apiKey) {
+    return <ErrorAlert error='Não foi possível encontrar a chave de API.' />
+  }
+
+  return (
+    <div>
+      <ApiKeyField apiKey={apiKey?.key || ''} />
+    </div>
+  )
 }
