1a530c404bf2ea109e39fbd0092d544a0fdfb271
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-09-30 23:16:33 -0300

    feat: improve metadata | add light and dark mode favicon

--- apps/app/src/app/layout.tsx
+++ apps/app/src/app/layout.tsx
@@ -11,9 +11,56 @@ const interFont = Inter({
   display: 'swap',
 })
 
+const title = 'Advents | Links únicos e mensuráveis para seus apps'
+const description =
+  'A Advents é a alternativa brasileira à AppsFlyer. Crie links únicos para seu apps Android e iOS. Meça o resultado das campanhas de instalação com nossa ferramenta de atribuição e analytics, e engaje seus usuários com deep links.'
+
 export const metadata: Metadata = {
-  title: 'Advents',
-  description: 'Deep links de forma fácil!',
+  title,
+  description,
+  alternates: {
+    canonical: 'https://app.advents.io',
+  },
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      url: '/favicon-light.png',
+      media: '(prefers-color-scheme: light)',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      url: '/favicon-dark.png',
+      media: '(prefers-color-scheme: dark)',
+    },
+  ],
+  metadataBase: new URL('https://app.advents.io'),
+  openGraph: {
+    type: 'website',
+    title,
+    description,
+    url: '/',
+    images: {
+      url: '/og.png',
+      type: 'image/png',
+      width: 1200,
+      height: 630,
+      alt: 'Advents',
+    },
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+    images: {
+      url: '/og.png',
+      type: 'image/png',
+      width: 1200,
+      height: 630,
+      alt: 'Advents',
+    },
+  },
 }
 
 export default async function RootLayout({
