01865ea0942411c14b656ecc99ab7be77613545e
Gabriel Moresco <gabriel.moresco25@gmail.com>
2024-11-08 14:39:23 -0300

    refac: change `queries` package to has a `client` queries and `server` queries

--- packages/queries/index.ts
+++ packages/queries/index.ts
@@ -1,19 +1,25 @@
 import { handle } from 'hono/vercel'
 
-import { api } from './src/api'
+import { api } from './src/client/api'
 
 export { api, handle }
 
-export type { GetAppInput, GetAppOutput } from './src/routes/get-app'
-export type { GetAppAnalyticsInput, GetAppAnalyticsOutput } from './src/routes/get-app-analytics'
+export type { GetAppInput, GetAppOutput } from './src/client/routes/get-app'
+export type {
+  GetAppAnalyticsInput,
+  GetAppAnalyticsOutput,
+} from './src/client/routes/get-app-analytics'
 export type {
   GetAppDefaultValuesInput,
   GetAppDefaultValuesOutput,
-} from './src/routes/get-app-default-values'
-export type { GetAppIdInput, GetAppIdOutput } from './src/routes/get-app-id'
-export type { GetAppQrCodeUrlInput, GetAppQrCodeUrlOutput } from './src/routes/get-app-qrcode-url'
-export type { GetLinkInput, GetLinkOutput } from './src/routes/get-link'
+} from './src/client/routes/get-app-default-values'
+export type { GetAppIdInput, GetAppIdOutput } from './src/client/routes/get-app-id'
+export type {
+  GetAppQrCodeUrlInput,
+  GetAppQrCodeUrlOutput,
+} from './src/client/routes/get-app-qrcode-url'
+export type { GetLinkInput, GetLinkOutput } from './src/client/routes/get-link'
 export type {
   GetLinksAnalyticsInput,
   GetLinksAnalyticsOutput,
-} from './src/routes/get-links-analytics'
+} from './src/client/routes/get-links-analytics'
