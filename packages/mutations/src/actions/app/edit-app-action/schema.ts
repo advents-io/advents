524fa29c3ffb2d7b17835a9451c438d9e63b7c39
Gabriel Moresco <gabriel.moresco25@gmail.com>
2025-01-22 13:54:44 -0300

    refac: reuse create app schema in edit app schema

--- packages/mutations/src/actions/app/edit-app-action/schema.ts
+++ packages/mutations/src/actions/app/edit-app-action/schema.ts
@@ -1,43 +1,10 @@
 import { z } from 'zod'
 
-import { regexes } from '../../../utils/regexes'
+import { createAppInputSchema } from '../create-app-action/schema'
 
-export const editAppFormInputSchema = z.object({
-  name: z
-    .string({ message: 'Nome do app é obrigatório.' })
-    .min(1, 'Nome do app é obrigatório.')
-    .max(64, 'O nome do app deve possuir no máximo 64 caracteres.'),
-  slug: z
-    .string({ message: 'Slug do app é obrigatório.' })
-    .min(1, 'Slug do app é obrigatório.')
-    .max(48, 'O Slug do app deve possuir no máximo 48 caracteres.')
-    .regex(
-      regexes.LOWER_CASE_SLUG,
-      'O Slug do app deve conter apenas letras minusculas, números, hífen ou underline.',
-    ),
+export const editAppFormInputSchema = createAppInputSchema.extend({
   defaultDomain: z.string({ message: 'Domínio inválido.' }),
-  androidUrl: z
-    .string({ message: 'Url inválida.' })
-    .url('Url inválida.')
-    .includes('play.google.com', {
-      message: 'A url do app Android deve ser da Google Play Store.',
-    })
-    .startsWith('https://', { message: 'A url deve ser https.' })
-    .startsWith('https://play.google.com/store/apps/details?', {
-      message: 'Url da Play Store inválida.',
-    }),
-  iosUrl: z
-    .string({ message: 'Url inválida.' })
-    .url('Url inválida.')
-    .includes('apps.apple.com', {
-      message: 'A url do app iOS deve ser da App Store.',
-    })
-    .startsWith('https://', { message: 'A url deve ser https.' })
-    .startsWith('https://apps.apple.com/', {
-      message: 'Url da App Store inválida.',
-    }),
   disableIosPreviewPage: z.boolean(),
-  fallbackUrl: z.string({ message: 'Url inválida.' }).url('Url inválida.'),
 })
 
 export const inputSchema = editAppFormInputSchema.extend({
