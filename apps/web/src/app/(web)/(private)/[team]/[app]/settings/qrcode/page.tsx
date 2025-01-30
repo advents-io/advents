7b2f5c30efcde14c2aaee00c4925eae15d1f5183
Gabriel Moresco <gabriel.moresco25@gmail.com>
2025-01-30 09:56:41 -0300

    refac: remove loading from qrcode page

--- apps/web/src/app/(web)/(private)/[team]/[app]/settings/qrcode/page.tsx
+++ apps/web/src/app/(web)/(private)/[team]/[app]/settings/qrcode/page.tsx
@@ -1,12 +1,5 @@
-import { Suspense } from 'react'
-
 import { EditAppQrCodeLogoForm } from './edit-app-qrcode-logo-form'
-import Loading from './loading'
 
 export default function Page() {
-  return (
-    <Suspense fallback={<Loading />}>
-      <EditAppQrCodeLogoForm />
-    </Suspense>
-  )
+  return <EditAppQrCodeLogoForm />
 }
